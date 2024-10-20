5112ce3ea705675eaad693a675b4723d84a37800
iamharshdeep <139531729+iamharshdeep@users.noreply.github.com>
2024-10-21 00:00:33 +0530

    Update scheduler.js

--- scheduler.js
+++ scheduler.js
@@ -1,9 +1,10 @@
 const schedule = require("node-schedule");
 const axios = require("axios");
 
-const job = schedule.scheduleJob("*/13 * * * *", async () => {
+const job = schedule.scheduleJob("*/1 * * * *", async () => {
     try {
         await axios.get("https://snp-backend-7xuw.onrender.com/");
+        await axios.get("https://health-ed-tech-backend-fg9u.onrender.com/");
     } catch (err) {
         console.log(err.message);
     }
